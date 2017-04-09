80f0ffa3a359955ab2e87f08f8056b9285fab281
DavidMBP <steadypathapp@gmail.com>
2017-04-09 12:53:06 -0500

    Added 2 new enemies to the enemies array

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = []
+var allEnemies = [
+  new Enemy(0, 2, 100),
+  new Enemy(0, 3, 1000)
+]
 var player = new Player(Player.INITIAL_X,Player.INITIAL_Y)
 
 
