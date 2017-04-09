8c42cb1f85a0ccc36cd4be1d33c20fcd604f2cba
DavidMBP <steadypathapp@gmail.com>
2017-04-09 13:02:21 -0500

    Added class comment

--- js/enemy.js
+++ js/enemy.js
@@ -1,8 +1,11 @@
-// Enemies our player must avoid
+/**
+* Creates an enemy with a row and velocity
+* @class
+* @param {number} x
+* @param {number} row - The row where the enemy lives
+* @param {number} velocity - The enemies velocity
+*/
 var Enemy = function(x, row, velocity) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -11,6 +14,7 @@ var Enemy = function(x, row, velocity) {
     this.velocity = velocity;
 };
 
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
@@ -18,12 +22,12 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     if(this.x + (this.velocity * dt) < GAME_WIDTH + Enemy.WIDTH){
-      this.x += (this.velocity * dt)
+      this.x += (this.velocity * dt);
     }else{
-      this.x = -Enemy.WIDTH
+      this.x = -Enemy.WIDTH;
     }
-
 };
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
